ed33dfcc3a33fc5ee2509c030c6e78dc5002f6da
Zen <muhzaini30@gmail.com>
2022-11-30 13:55:45 +0800

    Oke

---
+++ info-astronomy/olah.ts
@@ -0,0 +1,5 @@
+import { readFileSync, writeFileSync } from "https://deno.land/std/node/fs.ts";
+
+let filenya = Deno.args[0];
+filenya = readFileSync(filenya).toString();
+writeFileSync("file-baru.html", filenya);
